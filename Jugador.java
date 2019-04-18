90e6288d1191f87fb97c08cd05bae929dcf12e17
Adriana Hernández González <adri315289837@gmail.com>
2019-04-18 12:02:55 -0500

    Primer commit, empezando el proyecto

---
+++ Jugador.java
@@ -0,0 +1,28 @@
+public class Jugador{
+	String nombre;
+	char[] pociones;
+	char[] pokemones;
+
+	public int GuardarPokemon(){
+
+	}
+
+	public int ElegirPokemon(){
+
+	}
+
+	public int UsarPoción(){
+
+	}
+
+	public int ListarPokemon(){
+
+	} 
+}
+
+
+//??????????
+/*public int PocionVida(){
+		pokemon.vida=pokemon.vida*.20;
+	}
+*/
---
+++ Pokemon.java
@@ -0,0 +1,22 @@
+public class Pokemon {
+	String apodo;
+	float vida;
+	float defensa;
+	int estado; //(paralizado, quemado, envenenado, etc.).
+	int tipo; //(Agua, Fuego, Hierba y Eléctrico). (4)
+
+
+	public float RecibirDanio(){
+		 /*
+		• Fuego: x2 de daño a tipo Hierba, x0.5 a tipo Agua o Fuego, x1 a otro tipo.
+		• Agua: x2 de daño a tipo Fuego, x0.5 a tipo Agua o Hierba, x1 a otro tipo.
+		• Hierba: x2 de daño a tipo Agua, x0.5 a tipo Hierba o Fuego, x1 a otro tipo.
+		• Eléctrico: x2 de daño a tipo Agua, x0.5 a tipo Hierba o Eléctrico, x1 a otro tipo.
+		 */
+	}
+
+	public float  CambioPuntos(){ ///Recibir puntos de vida pociones, de ataque pociones y puntos defensar
+
+	}
+
+}
